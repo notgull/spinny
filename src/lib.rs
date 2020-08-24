62076f6d35a83c4bf3ef5f52a871b5be8438bf97
not_a_seagull <notaseagull048@gmail.com>
2020-08-23 22:22:54 -0700

    Needs tests

---
+++ src/lib.rs
@@ -0,0 +1,112 @@
+// MIT/Apache2 License
+
+//! Implementation of a basic spin-based RwLock
+
+#![no_std]
+#![warn(clippy::pedantic)]
+
+use core::sync::atomic::{spin_loop_hint, AtomicUsize, Ordering};
+use lock_api::{GuardSend, RawRwLock, RawRwLockDowngrade, RawRwLockUpgrade, RwLock as LARwLock};
+
+/// Raw spinlock rwlock, wrapped in the lock_api RwLock struct.
+pub struct RawRwSpinlock(AtomicUsize);
+
+// flags stored in the usize struct
+const READER: usize = 1 << 2;
+const UPGRADED: usize = 1 << 1;
+const WRITER: usize = 1 << 0;
+
+unsafe impl RawRwLock for RawRwSpinlock {
+    const INIT: RawRwSpinlock = RawRwSpinlock(AtomicUsize::new(0));
+
+    type GuardMarker = GuardSend;
+
+    fn lock_shared(&self) {
+        while !self.try_lock_shared() {
+            spin_loop_hint()
+        }
+    }
+
+    fn try_lock_shared(&self) -> bool {
+        let value = self.0.fetch_add(READER, Ordering::Acquire);
+
+        if value & (WRITER | UPGRADED) != 0 {
+            self.0.fetch_sub(READER, Ordering::Relaxed);
+            false
+        } else {
+            true
+        }
+    }
+
+    fn try_lock_exclusive(&self) -> bool {
+        self.0
+            .compare_exchange(0, WRITER, Ordering::Acquire, Ordering::Relaxed)
+            .is_ok()
+    }
+
+    fn lock_exclusive(&self) {
+        loop {
+            match self
+                .0
+                .compare_exchange_weak(0, WRITER, Ordering::Acquire, Ordering::Relaxed)
+            {
+                Ok(_) => return,
+                Err(_) => spin_loop_hint(),
+            }
+        }
+    }
+
+    unsafe fn unlock_shared(&self) {
+        self.0.fetch_sub(READER, Ordering::Release);
+    }
+
+    unsafe fn unlock_exclusive(&self) {
+        self.0.fetch_and(!(WRITER | UPGRADED), Ordering::Release);
+    }
+}
+
+unsafe impl RawRwLockUpgrade for RawRwSpinlock {
+    fn lock_upgradable(&self) {
+        while !self.try_lock_upgradable() {
+            spin_loop_hint()
+        }
+    }
+
+    fn try_lock_upgradable(&self) -> bool {
+        self.0.fetch_or(UPGRADED, Ordering::Acquire) & (WRITER | UPGRADED) == 0
+    }
+
+    unsafe fn try_upgrade(&self) -> bool {
+        self.0
+            .compare_exchange(UPGRADED, WRITER, Ordering::Acquire, Ordering::Relaxed)
+            .is_ok()
+    }
+
+    unsafe fn upgrade(&self) {
+        loop {
+            match self.0.compare_exchange_weak(
+                UPGRADED,
+                WRITER,
+                Ordering::Acquire,
+                Ordering::Relaxed,
+            ) {
+                Ok(_) => return,
+                Err(_) => spin_loop_hint(),
+            }
+        }
+    }
+
+    unsafe fn unlock_upgradable(&self) {
+        self.0.fetch_sub(UPGRADED, Ordering::AcqRel);
+    }
+}
+
+unsafe impl RawRwLockDowngrade for RawRwSpinlock {
+    unsafe fn downgrade(&self) {
+        self.0.fetch_add(READER, Ordering::Acquire);
+        self.unlock_exclusive();
+    }
+}
+
+/// A read-write lock that uses a spinlock internally.
+pub type RwLock<T> = LARwLock<T, RawRwSpinlock>;
