7871eb7c67d93e6155e0d8b9cc8ed36fe6a4c29e
not_a_seagull <notaseagull048@gmail.com>
2020-09-13 10:53:56 -0700

    Adding loom

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Provides an `RwLock`-like struct that is `no_std` compatible and based on spinlocks. Made this because I couldn't find an equivalent that already existed that wasn't unmaintained.
 
+# Thanks
+
+Thank you to mvdnes for creating the original `spin` crate, which this crate is inspired by.
+
 ## License
 
 Dual licensed under the MIT License and Apache-2.0 License at the user's option.
--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,9 @@
 
 #![no_std]
 #![warn(clippy::pedantic)]
+#![allow(clippy::same_item_push)]
 
-#[cfg(test)]
+#[cfg(any(test, loom))]
 extern crate std;
 
 use lock_api::{
@@ -18,9 +19,32 @@ use lock_api::{
 use core::sync::atomic::{spin_loop_hint, AtomicUsize, Ordering};
 #[cfg(loom)]
 use loom::sync::atomic::{spin_loop_hint, AtomicUsize, Ordering};
+#[cfg(loom)]
+use once_cell::sync::OnceCell;
 
-/// Raw spinlock rwlock, wrapped in the lock_api RwLock struct.
+#[cfg(not(loom))]
+/// Raw spinlock rwlock, wrapped in the `lock_api` RwLock struct.
 pub struct RawRwSpinlock(AtomicUsize);
+
+#[cfg(loom)]
+/// Raw spinlock rwlock, wrapped in the `lock_api` RwLock struct.
+pub struct RawRwSpinlock(OnceCell<AtomicUsize>);
+
+#[cfg(not(loom))]
+impl RawRwSpinlock {
+    #[inline]
+    fn ulock(&self) -> &AtomicUsize {
+        &self.0
+    }
+}
+
+#[cfg(loom)]
+impl RawRwSpinlock {
+    #[inline]
+    fn ulock(&self) -> &AtomicUsize {
+        self.0.get_or_init(|| AtomicUsize::new(0))
+    }
+}
 
 // flags stored in the usize struct
 const READER: usize = 1 << 2;
@@ -28,7 +52,10 @@ const UPGRADED: usize = 1 << 1;
 const WRITER: usize = 1 << 0;
 
 unsafe impl RawRwLock for RawRwSpinlock {
+    #[cfg(not(loom))]
     const INIT: RawRwSpinlock = RawRwSpinlock(AtomicUsize::new(0));
+    #[cfg(loom)]
+    const INIT: RawRwSpinlock = RawRwSpinlock(OnceCell::new());
 
     type GuardMarker = GuardSend;
 
@@ -39,10 +66,10 @@ unsafe impl RawRwLock for RawRwSpinlock {
     }
 
     fn try_lock_shared(&self) -> bool {
-        let value = self.0.fetch_add(READER, Ordering::Acquire);
+        let value = self.ulock().fetch_add(READER, Ordering::Acquire);
 
         if value & (WRITER | UPGRADED) != 0 {
-            self.0.fetch_sub(READER, Ordering::Relaxed);
+            self.ulock().fetch_sub(READER, Ordering::Relaxed);
             false
         } else {
             true
@@ -50,17 +77,19 @@ unsafe impl RawRwLock for RawRwSpinlock {
     }
 
     fn try_lock_exclusive(&self) -> bool {
-        self.0
+        self.ulock()
             .compare_exchange(0, WRITER, Ordering::Acquire, Ordering::Relaxed)
             .is_ok()
     }
 
     fn lock_exclusive(&self) {
         loop {
-            match self
-                .0
-                .compare_exchange_weak(0, WRITER, Ordering::Acquire, Ordering::Relaxed)
-            {
+            match self.ulock().compare_exchange_weak(
+                0,
+                WRITER,
+                Ordering::Acquire,
+                Ordering::Relaxed,
+            ) {
                 Ok(_) => return,
                 Err(_) => spin_loop_hint(),
             }
@@ -68,11 +97,12 @@ unsafe impl RawRwLock for RawRwSpinlock {
     }
 
     unsafe fn unlock_shared(&self) {
-        self.0.fetch_sub(READER, Ordering::Release);
+        self.ulock().fetch_sub(READER, Ordering::Release);
     }
 
     unsafe fn unlock_exclusive(&self) {
-        self.0.fetch_and(!(WRITER | UPGRADED), Ordering::Release);
+        self.ulock()
+            .fetch_and(!(WRITER | UPGRADED), Ordering::Release);
     }
 }
 
@@ -84,18 +114,18 @@ unsafe impl RawRwLockUpgrade for RawRwSpinlock {
     }
 
     fn try_lock_upgradable(&self) -> bool {
-        self.0.fetch_or(UPGRADED, Ordering::Acquire) & (WRITER | UPGRADED) == 0
+        self.ulock().fetch_or(UPGRADED, Ordering::Acquire) & (WRITER | UPGRADED) == 0
     }
 
     unsafe fn try_upgrade(&self) -> bool {
-        self.0
+        self.ulock()
             .compare_exchange(UPGRADED, WRITER, Ordering::Acquire, Ordering::Relaxed)
             .is_ok()
     }
 
     unsafe fn upgrade(&self) {
         loop {
-            match self.0.compare_exchange_weak(
+            match self.ulock().compare_exchange_weak(
                 UPGRADED,
                 WRITER,
                 Ordering::Acquire,
@@ -108,13 +138,13 @@ unsafe impl RawRwLockUpgrade for RawRwSpinlock {
     }
 
     unsafe fn unlock_upgradable(&self) {
-        self.0.fetch_sub(UPGRADED, Ordering::AcqRel);
+        self.ulock().fetch_sub(UPGRADED, Ordering::AcqRel);
     }
 }
 
 unsafe impl RawRwLockDowngrade for RawRwSpinlock {
     unsafe fn downgrade(&self) {
-        self.0.fetch_add(READER, Ordering::Acquire);
+        self.ulock().fetch_add(READER, Ordering::Acquire);
         self.unlock_exclusive();
     }
 }
@@ -138,22 +168,23 @@ fn basics() {
 
 #[cfg(test)]
 mod tests {
-    use super::{RwLock, RwLockReadGuard, RwLockUpgradableReadGuard, RwLockWriteGuard};
+    use super::{RwLock, RwLockUpgradableReadGuard};
 
     #[cfg(loom)]
     use loom::thread;
     #[cfg(not(loom))]
     use std::thread;
 
-    use std::vec::Vec;
+    use std::{sync::Arc, vec::Vec};
 
     // test multiple reads
     fn multiread_kernel() {
-        let rwlock = RwLock::new(7);
+        let rwlock = Arc::new(RwLock::new(7));
         let mut joiners = Vec::new();
-        for _ in 0..10 {
-            joiners.push(thread::spawn(|| {
-                let lock = rwlock.read();
+        for _ in 0..1 {
+            let rclone = rwlock.clone();
+            joiners.push(thread::spawn(move || {
+                let lock = rclone.read();
                 assert_eq!(*lock, 7);
             }));
         }
@@ -175,17 +206,18 @@ mod tests {
 
     // test multiple writes
     fn multiwrite_kernel() {
-        let rwlock = RwLock::new(0);
+        let rwlock = Arc::new(RwLock::new(0));
         let mut joiners = Vec::new();
-        for _ in 0..10 {
-            joiners.push(thread::spawn(|| {
-                let lock = rwlock.write();
+        for _ in 0..2 {
+            let rclone = rwlock.clone();
+            joiners.push(thread::spawn(move || {
+                let mut lock = rclone.write();
                 *lock += 1;
             }));
         }
 
         joiners.into_iter().for_each(|j| j.join().unwrap());
-        assert_eq!(*rwlock.read(), 10);
+        assert_eq!(*rwlock.read(), 2);
     }
 
     #[cfg(loom)]
@@ -202,15 +234,16 @@ mod tests {
 
     // test upgrading
     fn upgrade_kernel() {
-        let rwlock = RwLock::new((false, 0));
+        let rwlock = Arc::new(RwLock::new((false, 0)));
         let mut joiners = Vec::new();
-        for i in 0..12 {
-            joiners.push(thread::spawn(|| {
-                let lock = RwLock::upgradable_read(&rwlock);
+        for i in 0..2 {
+            let rclone = rwlock.clone();
+            joiners.push(thread::spawn(move || {
+                let lock = RwLock::upgradable_read(&rclone);
 
                 // even numbers just read the lock, determine the first element is false, then return
                 if i & 1 == 0 {
-                    assert_eq!(*lock.0, false);
+                    assert_eq!(lock.0, false);
                 } else {
                     // odd numbers increment the number
                     let mut lock = RwLockUpgradableReadGuard::upgrade(lock);
@@ -220,7 +253,7 @@ mod tests {
         }
 
         joiners.into_iter().for_each(|j| j.join().unwrap());
-        assert_eq!(rwlock.read().0, 6);
+        assert_eq!(rwlock.read().1, 1);
     }
 
     #[cfg(loom)]
